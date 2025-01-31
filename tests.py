a3f97e3f6bef6e39a65b74694d2e253f000575e6
Anna Slobodyanyuk <anne.slobodyanyuk@yandex.ru>
2025-01-31 15:27:49 +0300

    new_commit

--- tests.py
+++ tests.py
@@ -3,18 +3,6 @@ import pytest
 
 class TestBooksCollector:
 
-    def test_get_books_genre_true(self, collector):
-        assert collector.get_books_genre() == {}
-
-    def test_get_list_of_favorites_books_true(self, collector):
-        assert collector.get_list_of_favorites_books() == []
-
-    def test_genre_true(self, collector):
-        assert collector.genre == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
-
-    def test_genre_age_rating_true(self, collector):
-        assert collector.genre_age_rating == ['Ужасы', 'Детективы']
-
     @pytest.mark.parametrize('name', ['Я', 'Турецкий гамбит', 'Удивительные приключения Робинзона Крузо'])
     def test_add_new_book_one_book_true(self, name, collector):
         collector.add_new_book(name)
@@ -25,14 +13,32 @@ class TestBooksCollector:
         assert len(collector.get_books_genre()) == 0
 
     def test_set_book_genre_true(self, collector):
-        collector.books_genre['Турецкий гамбит'] = ''
+        collector.add_new_book('Турецкий гамбит')
         collector.set_book_genre('Турецкий гамбит', 'Детективы')
         assert collector.get_book_genre('Турецкий гамбит') == 'Детективы'
+
+    def test_get_book_genre_true(self, collector):
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert collector.get_book_genre('Оно') == 'Ужасы'
 
     def test_get_books_with_specific_genre_true(self, collector):
         collector.add_new_book('Властелин колец')
         collector.set_book_genre('Властелин колец', 'Фантастика')
-        assert len(collector.get_books_with_specific_genre('Фантастика')) == 1
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert 'Оно' not in collector.get_books_with_specific_genre('Фантастика')
+
+    def test_get_books_genre(self, collector):
+        collector.add_new_book('Турецкий гамбит')
+        collector.set_book_genre('Турецкий гамбит', 'Детективы')
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert len(collector.get_books_genre()) == 3
 
     def test_get_books_for_children_true(self, collector):
         collector.add_new_book('Турецкий гамбит')
@@ -53,3 +59,11 @@ class TestBooksCollector:
             collector.add_book_in_favorites(book)
         collector.delete_book_from_favorites('Турецкий гамбит')
         assert 'Турецкий гамбит' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_true(self, collector):
+        collector.add_new_book('Турецкий гамбит')
+        collector.add_new_book('Властелин колец')
+        collector.add_new_book('Гарри Поттер')
+        for book in collector.get_books_genre():
+            collector.add_book_in_favorites(book)
+        assert collector.get_list_of_favorites_books() == ['Турецкий гамбит', 'Властелин колец', 'Гарри Поттер']
